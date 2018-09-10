9a7a783fb207cfe3ef91987017077796333410e7
Misael Jr <juniomisael@gmail.com>
2018-09-10 13:26:41 -0300

    Update sum_matrix_omp_for.c

--- sum_matrix_omp_for.c
+++ sum_matrix_omp_for.c
@@ -2,7 +2,8 @@
 Misael Jr 
 Phd Student
 ICMC - USP
-execute: clang sum_matrix_omp_for.c -fopenmp -o sum_matrix_omp_for
+execute on mac: clang sum_matrix_omp_for.c -fopenmp -o sum_matrix_omp_for
+execute on linux: gcc -fopenmp sum_matrix_omp_for.c -o sum_matrix_omp_for
 */
 
 #include <stdio.h>
